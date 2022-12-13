add178811a4c70c212afd379971f27376c9e84a1
SK ISMILE <101392872+skismile@users.noreply.github.com>
2022-12-13 20:38:16 +0530

    add

--- Routes/UserRegister.js
+++ Routes/UserRegister.js
@@ -10,27 +10,27 @@ app.post("/create", async (req, res) => {
     res.status(401).send(error.message);
   }
 });
-app.post("/get", async (req, res) => {
-  const { id } = req.body;
-  try {
-    const add = await UserModel.findOne({ _id: id });
-    res.send(add);
-  } catch (error) {
-    res.status(401).send(error.message);
-  }
-});
-app.post("/login", async (req, res) => {
-  const { email, name, password } = req.body;
-  try {
-    const respo = await UserModel.findOne({ name, email, password });
+// app.post("/get", async (req, res) => {
+//   const { id } = req.body;
+//   try {
+//     const add = await UserModel.findOne({ _id: id });
+//     res.send(add);
+//   } catch (error) {
+//     res.status(401).send(error.message);
+//   }
+// });
+// app.post("/login", async (req, res) => {
+//   const { email, name, password } = req.body;
+//   try {
+//     const respo = await UserModel.findOne({ name, email, password });
 
-    if (!respo.name) {
-      res.status(404).send("User not found");
-    } else {
-      res.status(200).send(respo);
-    }
-  } catch (error) {
-    res.status(404).send("User not found");
-  }
-});
+//     if (!respo.name) {
+//       res.status(404).send("User not found");
+//     } else {
+//       res.status(200).send(respo);
+//     }
+//   } catch (error) {
+//     res.status(404).send("User not found");
+//   }
+// });
 module.exports = app;
--- server.js
+++ server.js
@@ -10,20 +10,7 @@ app.use("/auth", userRegister);
 const PORT = process.env.PORT || 8080;
 mongoose.set("strictQuery", false);
 const MONGOURL = process.env.MONGO_URL;
-// app.post("/emi", async (req, res) => {
-//   let { loanamount, annualinterest, tenure } = req.body;
-//   let rate = annualinterest / 12 / 100;
-//   rate = rate.toFixed(6);
-//   let months = tenure;
 
-//   let EMI =
-//     (loanamount * rate * Math.pow(1 + rate, months)) /
-//     (Math.pow(1 + rate, months) - 1);
-//   EMI = Math.round(EMI);
-//   let totalPayment = EMI * months;
-//   let interestPayable = totalPayment - loanamount;
-//   res.send({ EMI, totalPayment, interestPayable });
-// });
 app.get("/", (req, res) => {
   res.send("hey home");
 });
